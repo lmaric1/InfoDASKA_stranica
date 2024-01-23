9c62f984437d092a109c79f4d501a13ce5b22d0e
lmaric1 <93379077+lmaric1@users.noreply.github.com>
2024-01-23 16:24:23 +0100

    Radi fotogalerija

---
+++ script.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    $('.fotorama').fotorama({
+        width: '900px',
+        ratio: '700/467',
+        maxwidth: '100%',
+        transition: 'dissolve', 
+        autoplay: 3000 
+    });
+});
+
+function scrollToTop() {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+}
